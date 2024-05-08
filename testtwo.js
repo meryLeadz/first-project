0dd32503b62cdf435cb321cfd761ce2b859821de
pknowledge <maryem.saad@leadz.ma>
2024-05-08 17:53:52 +0100

    toto

---
+++ testtwo.js
@@ -0,0 +1,9 @@
+
+<script>
+function myFunction() {
+  const inpObj = document.getElementById("id1");
+  if (!inpObj.checkValidity()) {
+    document.getElementById("demo").innerHTML = inpObj.validationMessage;
+  }
+}
+</script>
